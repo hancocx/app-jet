75e75964631a8b3cbc99b464de3e54f578b5f30b
Ricardo Zarate <ricardozc.contacto@gmail.com>
2022-09-03 10:43:13 -0500

    Se carga plantilla AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])
+        ->group(function () {
+
+            Route::get('/dash', function () {
+        return view('dash.index');
+    })->name('dash');
 });
